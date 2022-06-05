9ec554b239917433b8b7c839d27e6c7ec4c4a99c
Tomasz Kunicki <tomekkunicki7@gmail.com>
2022-06-05 15:04:09 +0200

    commit 2

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
-import sqlite3
-import csv
+import csv, sqlite3
+
+if __name__ == "__main__":
+  sql_con= sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+  cur = sql_con.cursor()
+  cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
+                    to_subscriber data_type INTEGER, 
+                    datetime data_type timestamp, 
+                    duration data_type INTEGER , 
+                    celltower data_type INTEGER);''') 
+
+  with open('polaczenia_duze.csv','r') as fin: 
+      reader = csv.reader(fin, delimiter = ";") 
+      next(reader, None)
+      rows = [x for x in reader]
+      cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?)", rows,)
+      sqlite_con.commit()
 
 
-class DurationCalculator:
+    cur.execute("Select sum(duration) from polaczenia")
+    result = cur.fetchone()[0]
 
-    def __init__(self) -> None:
-        self.sqlite_con = sqlite_con = sqlite3.connect(":memory:",
-                                                       detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-        self.cur = sqlite_con.cursor()
-
-    def create_table(self):
-        self.cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
-                          to_subscriber data_type INTEGER, 
-                          datetime data_type timestamp, 
-                          duration data_type INTEGER , 
-                          celltower data_type INTEGER);''')
-
-    def insert_csv(self):
-        with open('polaczenia_duze.csv', 'r') as fin:
-            # csv.DictReader uses first line in file for column headings by default
-            reader = csv.reader(fin, delimiter=";")  # comma is default delimiter
-            next(reader, None)  # skip the headers
-            rows = [x for x in reader]
-            self.cur.executemany(
-                "INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration, celltower) VALUES (?, ?, ?, ?, ?);",
-                rows)
-            self.sqlite_con.commit()
-
-    def calculate_duration_sum(self):
-        self.cur.execute('SELECT duration FROM polaczenia')
-        val = self.cur.fetchall()
-        sum = 0
-        for element in val:
-            for element2 in element:
-                sum += element2
-        return sum
-
-
-duration_calculator = DurationCalculator
-duration_calculator.create_table()
-duration_calculator.insert_csv()
-print(duration_calculator.calculate_duration_sum())
+print(str(result))
