8aa6a25cde52403b6202c954382068cac8107ed9
Tomasz Kunicki <tomekkunicki7@gmail.com>
2022-06-05 15:10:11 +0200

    commit 3

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
                     duration data_type INTEGER , 
                     celltower data_type INTEGER);''') 
 
-  with open('polaczenia_duze.csv','r') as fin: 
-      reader = csv.reader(fin, delimiter = ";") 
-      next(reader, None)
-      rows = [x for x in reader]
-      cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?)", rows,)
-      sqlite_con.commit()
+    with open('polaczenia_duze.csv','r') as fin: 
+        reader = csv.reader(fin, delimiter = ";")
+        next(reader, None)  
+        rows = [x for x in reader]
+        cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);",  rows)        
+        
+  sql_con
+  cursor=sql_con.cursor()
+  cursor.execute("Select sum(duration) from polaczenia")
+  result = cursor.fetchone()[0]
 
-
-    cur.execute("Select sum(duration) from polaczenia")
-    result = cur.fetchone()[0]
-
-print(str(result))
+print(str(result)
